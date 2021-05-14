85983e485af9bda1c9c292b7949727ead3b25ff0
DANG Truong <dangtruong080593@gmail.com>
2021-05-14 22:07:58 +0700

    reorganize big main class to multiple junit test cases

---
+++ src/test/Java/TestSortingAlgorithms.java
@@ -0,0 +1,26 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestSortingAlgorithms {
+
+    private static final int[] arrayToTest   = new int[]{12, 2, 8, 5, 1, 6, 4, 15};
+    private static final int[] expectedArray = new int[]{1, 2, 4, 5, 6, 8, 12, 15};
+
+    @Test
+    public void selectionSortTest() {
+        final int[] array = arrayToTest;
+
+        SortingAlgorithms.selectionSort(array);
+        Assert.assertArrayEquals(expectedArray, array);
+        SortingAlgorithms.print(array);
+    }
+
+    @Test
+    public void insertionSortTest() {
+        final int[] array = arrayToTest;
+
+        SortingAlgorithms.insertionSort(array);
+        Assert.assertArrayEquals(expectedArray, array);
+        SortingAlgorithms.print(array);
+    }
+}
