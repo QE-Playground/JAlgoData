1383ab33175cdac02e274e10a436de053c86345b
DANG Truong <dangtruong080593@gmail.com>
2021-05-14 21:53:05 +0700

    reorganize big main class to multiple junit test cases

---
+++ src/test/Java/TestSearchAlgorithms.java
@@ -0,0 +1,32 @@
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestSearchAlgorithms {
+
+    private static int[] array;
+    private static int elementToSearch;
+
+    @BeforeClass
+    public static void setUp() {
+        elementToSearch = 8;
+    }
+
+    @Test
+    public void linearSearchTest() {
+        array = new int[]{12, 2, 8, 5, 1, 6, 4, 15};
+        int index = SearchAlgorithms.linearSearch(array, elementToSearch);
+
+        Assert.assertEquals(2, index);
+        SearchAlgorithms.print(elementToSearch, index);
+    }
+
+    @Test
+    public void binarySearchTest() {
+        array = new int[]{1, 2, 4, 5, 6, 8, 12, 15};
+        int index = SearchAlgorithms.binarySearch(array, elementToSearch);
+
+        Assert.assertEquals(5, index);
+        SearchAlgorithms.print(elementToSearch, index);
+    }
+}
